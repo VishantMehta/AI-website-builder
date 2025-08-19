21d112648d617faefe0aeca5e634674b4641c3c7
Vishant <vishantmehta0@gmail.com>
2025-08-20 01:42:38 +0530

    initial auth setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-// backend/server.js
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db'); //for db
+const userRoutes = require('./routes/userRoutes');
 dotenv.config();
+connectDB(); //for db
 const app = express();
 const PORT = process.env.PORT || 5001;
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Hello from the backend server!' });
 });
+app.use('/api/users', userRoutes);
 app.listen(PORT, () => {
     console.log(`Backend server is running on http://localhost:${PORT}`);
 });
